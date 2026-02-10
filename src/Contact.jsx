2d29d7b5a2e143912545c02cc35f1f4236fff8e9
AntonyLeclerc <ptibaka@DESKTOP-3GSMA1J>
2026-02-10 15:40:33 +0100

    fix images again

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
       <Media
         above_text="Vous pouvez retrouver mes projets universitaires sur Github"
         id="github_contact"
-        image="src/assets/github-white-icon.webp"
+        image="/logos/github-white-icon.webp"
         text="Github"
         linkto="https://github.com/AntonyLeclerc"
       />
@@ -15,7 +15,7 @@ export default function Contact() {
       <Media
         above_text="Aller consulter ma page Linkedin"
         id="linkedin_contact"
-        image="src/assets/linkedin-app-white-icon.webp"
+        image="/logos/linkedin-app-white-icon.webp"
         text="Linkedin"
         linkto="https://www.linkedin.com/in/antony-leclerc-44848b255/"
       />
@@ -23,7 +23,7 @@ export default function Contact() {
       <Media
         above_text="Ou me contacter par email !"
         id="email_contact"
-        image="src/assets/email-envelope-white-icon.webp"
+        image="/logos/email-envelope-white-icon.webp"
         text="Email"
         linkto="mailto:leclerc.antony.pro@gmail.com"
       />
